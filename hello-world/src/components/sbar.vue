593e8129bc9cc9dfba0afbf25cd52a3459dd5126
ss891004 <575726653@qq.com>
2021-11-28 22:17:14 +0800

    update

---
+++ hello-world/src/components/sbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 class="sbar">Hello,{{ msg }}!(sBar)</h1>
+  </div>
+</template>
+
+<script>
+export default 
+{
+  name: 'sBar', 
+  //  不要使用下划线命名 s_Bar,因为要在标签中使用
+  props:
+  {
+    msg:String
+  }
+}
+</script>
+
+<style scoped lang='less'>
+// .sbar
+// {
+//     background: bisque;
+// }
+h1{background: orange;}
+</style>
