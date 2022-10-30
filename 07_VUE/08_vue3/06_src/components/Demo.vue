3fbfce40091978ffc8ec93ee76a5336b40bff257
ss891004 <575726653@qq.com>
2022-10-30 23:09:25 +0800

    update

--- 07_VUE/08_vue3/06_src/components/Demo.vue
+++ 07_VUE/08_vue3/06_src/components/Demo.vue
@@ -1,10 +1,10 @@
 <template>
-	<div>
-  <h1>一个人的信息</h1>
-  <h2>姓名：{{ person.name }}</h2>
-  <h2>年龄：{{ person.age }}</h2>
-  <button @click="test">测试触发一下Demo组件的Hello事件</button>
-</div>
+  <div>
+    <h1>一个人的信息</h1>
+    <h2>姓名：{{ person.name }}</h2>
+    <h2>年龄：{{ person.age }}</h2>
+    <button @click="test">测试触发一下Demo组件的Hello事件</button>
+  </div>
 </template>
 
 <script>
